66bfdd32d5da213f72f1f9836600737b73a82b07
Juliano <kluge233@hotmai.com>
2024-09-07 23:13:06 -0300

    ciado primeira versão do database service

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -1,1 +1,29 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+class DatabaseService {
+  static final DatabaseService _instance = DatabaseService._internal();
+  Database? _database;
+
+  DatabaseService._internal();
+
+  factory DatabaseService() {
+    return _instance;
+  }
+
+  Future<Database> get database async {
+    if (_database != null) return database!;
+    _database = await _initDatabase();
+
+    return database;
+  }
+}
+
+Future<Database> _initDatabase() async {
+  String path = join(await getDatabasesPath(), 'produtos.db');
+  return await openDatabase(path, version: 1, onCreate: (db, version) async {
+    await db.execute(
+      'CREATE TABLE produtos(codigo TEXT PRIMARY KEY, descricao TEXT, complemento TEXT, marca TEXT)',
+    );
+  });
+}
